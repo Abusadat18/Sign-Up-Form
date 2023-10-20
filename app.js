356001147f103d8fb6247c610c502f9fc931463a
Abu Sadat Ansari <itsabusadat@gmail.com>
2023-10-20 23:32:18 +0530

    Complete Sign Up form code + design

---
+++ app.js
@@ -0,0 +1,28 @@
+const password = document.getElementById("password");
+const rePassword = document.getElementById("rePassword");
+const createBtn = document.querySelector(".create-btn");
+const warningText = document.querySelector(".warning");
+
+let passwordValue;
+let rePasswordValue;
+
+password.addEventListener("keydown", function () {
+    passwordValue = password.value;
+})
+
+rePassword.addEventListener("keydown", function () {
+    rePasswordValue = rePassword.value;
+})
+
+createBtn.addEventListener("click",function(){
+if (passwordValue === rePasswordValue) {
+    password.classList.remove("error");
+    rePassword.classList.remove("error");
+    warningText.classList.remove("show")
+} else {
+    password.classList.add("error");
+    rePassword.classList.add("error");
+    warningText.classList.add("show")
+}
+})
+
